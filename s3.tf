4c6dc2559e5d24a8810498c5e3360d418206a648
DivijJ2 <divij.jain@capgemini.com>
2026-04-13 15:20:10 -0500

    Add files via upload

---
+++ s3.tf
@@ -0,0 +1,23 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-state-bucket-202611"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "state_block" {
+  bucket = aws_s3_bucket.terraform_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+  
+}
